59094ca2596b048df8ddcea87710199e95217da1
Pavel web2cap.ru <web2cap@gmail.com>
2022-01-09 18:36:38 +0300

    add login url and template

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,7 +1,7 @@
 from . import views
 
 # Импортируем из приложения django.contrib.auth нужный view-класс
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView
 from django.urls import path
 
 app_name = "users"
@@ -13,4 +13,9 @@ urlpatterns = [
         name="logout",
     ),
     path("signup/", views.SignUp.as_view(), name="signup"),
+    # :TODO add template for fogot password
+    path(
+        "password_reset_form/", PasswordResetView.as_view(), name="password_reset_form"
+    ),
+    path("login/", LoginView.as_view(template_name="users/login.html"), name="login"),
 ]
