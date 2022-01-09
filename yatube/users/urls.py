64dc5c599c20dfd763271a39f9a795b4fe6c4f7d
Cap Pavel <cap@Caps-MacBook-Air.local>
2022-01-09 13:59:45 +0300

    Add urls and register template for users

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,3 +1,5 @@
+from . import views
+
 # Импортируем из приложения django.contrib.auth нужный view-класс
 from django.contrib.auth.views import LogoutView
 from django.urls import path
@@ -10,4 +12,5 @@ urlpatterns = [
         LogoutView.as_view(template_name="users/logged_out.html"),
         name="logout",
     ),
+    path("signup/", views.SignUp.as_view(), name="signup"),
 ]
