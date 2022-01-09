b86948638fb629e08160121db4ea35d3ee552f24
Cap Pavel <cap@Caps-MacBook-Air.local>
2022-01-09 13:39:15 +0300

    Add urls and loguot template for users

---
+++ yatube/users/urls.py
@@ -0,0 +1,13 @@
+# Импортируем из приложения django.contrib.auth нужный view-класс
+from django.contrib.auth.views import LogoutView
+from django.urls import path
+
+app_name = "users"
+
+urlpatterns = [
+    path(
+        "logout/",
+        LogoutView.as_view(template_name="users/logged_out.html"),
+        name="logout",
+    ),
+]
