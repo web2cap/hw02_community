a11b314111141ec594c95f50f78e9bff93cc50de
Pavel web2cap.ru <web2cap@gmail.com>
2022-01-17 17:17:51 +0300

    add about app and 2 about pages

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "posts.apps.PostsConfig",
     "users.apps.UsersConfig",
+    "about.apps.AboutConfig",
     "core.apps.CoreConfig",
     "django.contrib.admin",
     "django.contrib.auth",
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path("", include("posts.urls", namespace="posts")),
     path("auth/", include("users.urls", namespace="users")),
     path("auth/", include("django.contrib.auth.urls")),
+    path("about/", include("about.urls", namespace="about")),
     path("admin/", admin.site.urls),
 ]
